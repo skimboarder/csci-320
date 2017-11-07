403c4c4a9cd13c7551152345b68fa000e995708e
p320-01c <p320-01c@cs.rit.edu>
2017-11-06 22:07:40 -0500

    finally done!

--- Test.java
+++ Test.java
@@ -326,6 +326,8 @@ public class Test {
 
     };
 
+    public static String[] STORED_PROCS = { "create or replace function calculate_profits(id int) returns money as $$ BEGIN return (SELEct sum(PRICE) FROM SALE WHERE EMP_ID = ID) * (SElect commission from employee where emp_id = id) / 100; END; $$ language plpgsql;" };
+
     public static int CAR_YEAR_START = 1997;
     public static int CAR_YEAR_END = 2017;
   
@@ -388,6 +390,13 @@ public class Test {
         populateCars();
         populateSalesPaymentsAndCustoms();
         giveCarsFeatures();
+        createStoredProcs();
+    }
+    
+    public static void createStoredProcs() {
+         for(String s : STORED_PROCS) {
+             System.out.println(s);
+         }
     }
 
     public static void createTriggerFunctions() {
